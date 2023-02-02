af25d666ebc3823c6cb869e87929b6ff085cd6fa
Johannes Moe <johannesmoehrl@protonmail.com>
2023-02-02 11:44:30 +0100

    added npm timer, not working

--- src/script/onoff.js
+++ src/script/onoff.js
@@ -1,3 +1,5 @@
+import { Timer, Time, TimerOptions } from 'timer-node';
+
 let black = '#1d1f21';
 let white = '#d9d9d9';
 let red = '#d60000';
@@ -9,21 +11,18 @@ let offBtn = document.getElementById('off');
 let usedTime = document.getElementById('z-n');
 
 let pause = true;
-let timer = false;
-let hour = '00';
-let minute = '00';
-let second = '00';
-let count = '00';
 
-let timestring;
+const timer = new Timer({ label: 'test-timer' });
 
 document.addEventListener('keydown', function (event) {
   if (event.keyCode == 38) {
     onBtn.style.background = green;
     offBtn.style.background = white;
-    timer = true;
-    stopWatch();
+    timer.start();
+    usedTime.innerHTML = timer.time();
+    console.log(timer.isStarted());
   } else if (event.keyCode == 40) {
+    console.log(timer.isStopped());
     offBtn.style.background = red;
     onBtn.style.background = white;
   } else if (event.keyCode == 39 && pause) {
@@ -32,45 +31,6 @@ document.addEventListener('keydown', function (event) {
   } else if (event.keyCode == 39 && pause == false) {
     usedTime.style.background = white;
     pause = true;
+    timer.resume();
   }
 });
-
-function stopWatch() {
-  if (timer) {
-    count++;
-    if (count == 100) {
-      second++;
-      count = 0;
-    }
-    if (second == 60) {
-      minute++;
-      second = 0;
-    }
-    if (minute == 60) {
-      hour++;
-      minute = 0;
-      second = 0;
-    }
-    let hrString = hour;
-    let minString = minute;
-    let secString = second;
-    let countString = count;
-    if (hour < 10) {
-      hrString = '0' + hrString;
-    }
-    if (minute < 10) {
-      minString = '0' + minString;
-    }
-    if (second < 10) {
-      secString = '0' + secString;
-    }
-    if (count < 10) {
-      countString = '0' + countString;
-    }
-
-    timestring =
-      hrString + ':' + minString + ':' + secString + ':' + countString;
-    usedTime.innerHTML = timestring;
-    setTimeout(stopWatch, 10);
-  }
-}
