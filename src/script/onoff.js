5e1351cedd8acc9c2f5dd920b793f474d2426b70
Johannes Moe <johannesmoehrl@protonmail.com>
2023-02-03 12:26:54 +0100

    added npm timer, working, als nächstes Zeitschupfen

--- src/script/onoff.js
+++ src/script/onoff.js
@@ -3,34 +3,39 @@ import { Timer, Time, TimerOptions } from 'timer-node';
 let black = '#1d1f21';
 let white = '#d9d9d9';
 let red = '#d60000';
-let green = '#36CF00';
+let green = '#13b010';
 let orange = '#fcba03';
 
-let onBtn = document.getElementById('on');
-let offBtn = document.getElementById('off');
 let usedTime = document.getElementById('z-n');
+let totalTime = document.getElementById('z-g');
 
 let pause = true;
 
-const timer = new Timer({ label: 'test-timer' });
+const timer = new Timer({});
 
 document.addEventListener('keydown', function (event) {
   if (event.keyCode == 38) {
-    onBtn.style.background = green;
-    offBtn.style.background = white;
+    usedTime.style.background = green;
     timer.start();
-    usedTime.innerHTML = timer.time();
     console.log(timer.isStarted());
+    usedTime.innerHTML = '';
   } else if (event.keyCode == 40) {
+    usedTime.style.background = red;
+    timer.stop();
     console.log(timer.isStopped());
-    offBtn.style.background = red;
-    onBtn.style.background = white;
+    usedTime.innerHTML = timer.format('%hh%mm%ss%msms');
   } else if (event.keyCode == 39 && pause) {
     usedTime.style.background = orange;
+    timer.pause();
+    usedTime.innerHTML = timer.format('%hh%mm%ss%msms');
     pause = false;
   } else if (event.keyCode == 39 && pause == false) {
-    usedTime.style.background = white;
+    usedTime.style.background = green;
     pause = true;
     timer.resume();
+    usedTime.innerHTML = '';
+  }
+  if (event.keyCode == 37) {
+    totalTime.style.background = green;
   }
 });
